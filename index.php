924b549bdee8acd64934c88c94763b103921e575
Vince Mendella <vmendella@gmail.com>
2022-04-18 17:08:29 -0500

    update files

--- index.php
+++ index.php
@@ -3,9 +3,7 @@
 <?php if(have_posts()) { ?>
   <?php while(have_posts()) { ?>
     <?php the_post(); ?>
-    <h2>
-      <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
-    </h2>
+
     <div>
       <?php _themename_post_meta(); ?>
     </div>
